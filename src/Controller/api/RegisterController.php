7342ee68f3a3cec2a90e752e77ebab40e3f2f48d
skifahrer <richard.dvorsky@bratislava.sk>
2024-11-05 23:15:34 +0100

    WIP - entities and migrations

--- src/Controller/api/RegisterController.php
+++ src/Controller/api/RegisterController.php
@@ -1,27 +1,34 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\api;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use App\Repository\UserRepository;
 
 class RegisterController extends AbstractController
 {
-    private UserPasswordHasherInterface $passwordHasher;
     private EntityManagerInterface $entityManager;
     private JWTTokenManagerInterface $jwtManager;
+    private UserRepository $userRepository;
+    private SessionInterface $session;
 
-    public function __construct(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager, JWTTokenManagerInterface $jwtManager)
-    {
-        $this->passwordHasher = $passwordHasher;
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        JWTTokenManagerInterface $jwtManager,
+        UserRepository $userRepository,
+        SessionInterface $session
+    ) {
         $this->entityManager = $entityManager;
         $this->jwtManager = $jwtManager;
+        $this->userRepository = $userRepository;
+        $this->session = $session;
     }
 
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
@@ -33,9 +40,14 @@ class RegisterController extends AbstractController
             return $this->json(['error' => 'Email and password are required.'], Response::HTTP_BAD_REQUEST);
         }
 
+        $existingUser = $this->userRepository->findOneBy(['email' => $data['email']]);
+        if ($existingUser) {
+            return $this->json(['error' => 'Email already exists.'], Response::HTTP_CONFLICT);
+        }
+
         $user = new User();
         $user->setEmail($data['email']);
-        $user->setPassword($this->passwordHasher->hashPassword($user, $data['password']));
+        $user->setPassword($data['password']);
         $user->setRoles(['ROLE_USER']);
 
         $this->entityManager->persist($user);
@@ -43,6 +55,16 @@ class RegisterController extends AbstractController
 
         $token = $this->jwtManager->create($user);
 
-        return $this->json(['token' => $token], Response::HTTP_CREATED);
+        // Save the JWT token to the session
+        $this->session->set('jwt', $token);
+
+        return $this->json([
+            'message' => 'User registered successfully',
+            'user' => [
+                'email' => $user->getEmail(),
+                'roles' => $user->getRoles(),
+            ],
+            'jwt' => $token
+        ], Response::HTTP_CREATED);
     }
 }
